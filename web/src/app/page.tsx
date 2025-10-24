a49982a267882320570446ad697aa9fd8c6937f2
Hannah Clay <hclay116@gmail.com>
2025-10-24 02:01:35 -0700

    removed notif counter

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -672,11 +672,6 @@ export default function Home() {
         >
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
         </svg>
-        {favorites.length > 0 && (
-          <span className="absolute -top-1 -right-1 w-5 h-5 bg-red-500 text-white text-xs rounded-full flex items-center justify-center font-bold">
-            {favorites.length}
-          </span>
-        )}
       </button>
 
     </div>
